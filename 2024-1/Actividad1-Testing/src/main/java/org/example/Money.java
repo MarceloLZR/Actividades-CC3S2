0f4931f40071bae238b1fba042e00aabd10538be
MarceloLZR <gerson.nunez.h@uni.pe>
2024-04-06 11:43:19 -0500

    cambios clase

--- 2024-1/Actividad1-Testing/src/main/java/org/example/Money.java
+++ 2024-1/Actividad1-Testing/src/main/java/org/example/Money.java
@@ -4,7 +4,14 @@ public class Money {
     private final int amount;
     private final String currency;
     public Money(int amount, String currency) {
+        if(amount<0){
+            throw new IllegalArgumentException(
+                    "Illegal amount:("+amount+")");
+
+        }
         this.amount = amount;
+
+
         this.currency = currency;
     }
     public int getAmount() {
--- 2024-1/Actividad1-Testing/src/test/java/org/example/MoneyTest.java
+++ 2024-1/Actividad1-Testing/src/test/java/org/example/MoneyTest.java
@@ -1,11 +1,17 @@
 package org.example;
-import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;//permite la anotacion@test de la linea test de la linea 6
+import static org.assertj.core.api.Assertions.assertThat;//importacion estatica del metodo asertThat de la clase aser asetrion
 
-class MoneyTest {
+public classMoneyTest {//
     @Test
-    void constructorShouldSetAmountAndCurrency() {
-        // Completa
+    void constructorShouldSetAmountAndCurrency() {//metodo de prueba
+
+        Money money= new Money(10,"USD");//SUT
+
+
+        assertThat(money.getAmount()).isEqualTo(10);//el sut se pone aa prueba y los resultados se ponen a prueba con la clase assert
+        assertThat(money.getCurrency()).isEqualTo("USD");
+
     }
 
 }
