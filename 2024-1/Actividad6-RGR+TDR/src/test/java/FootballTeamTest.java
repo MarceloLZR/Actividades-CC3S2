49a0a5c3959904f6656ceee3adda54b04e8ea1fc
MarceloLZR <gerson.nunez.h@uni.pe>
2024-04-17 09:50:29 -0500

    refactaoriza

--- 2024-1/Actividad6-RGR+TDR/src/test/java/FootballTeamTest.java
+++ 2024-1/Actividad6-RGR+TDR/src/test/java/FootballTeamTest.java
@@ -1,16 +1,17 @@
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
+/*
 public class FootballTeamTest {
     @Test
     void constructorShouldSetGamesWon() {
         FootballTeam team = new FootballTeam(3);
-        //assertThat(team.getGamesWon()).isEqualTo(3);
+        assertThat(team.getGamesWon()).isEqualTo(3);
         assertThat(team.getGamesWon())
                 .as("number of games won")
                 .isEqualTo(3);
     }
-}
-/*
+}*/
+//REFACTORIZAMOS EL CODIGO
 public class FootballTeamTest {
     private static final int THREE_GAMES_WON = 3;
     @Test
@@ -21,4 +22,3 @@ public class FootballTeamTest {
             .isEqualTo(THREE_GAMES_WON);
        }
 }
- */
