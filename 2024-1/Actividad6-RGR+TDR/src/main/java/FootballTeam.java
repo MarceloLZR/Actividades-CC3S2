b37b6e44548ed9c80ce13100426f47e67f66c3df
MarceloLZR <gerson.nunez.h@uni.pe>
2024-04-24 07:51:32 -0500

    6

--- 2024-1/Actividad6-RGR+TDR/src/main/java/FootballTeam.java
+++ 2024-1/Actividad6-RGR+TDR/src/main/java/FootballTeam.java
@@ -1,8 +1,62 @@
+
+/*
 public class FootballTeam {
+
     public FootballTeam(int gamesWon) {
     }
     public int getGamesWon() {
         //return 0;
         return 3;
     }
+}
+
+ */
+//PRUEBA PARAMETRIZADA
+/*
+public class FootballTeam {
+    private int gamesWon;
+
+    public FootballTeam(int nbOfGamesWon) {
+        this.gamesWon = nbOfGamesWon;
+    }
+
+    public int getGamesWon() {
+        return gamesWon;
+    }
+}
+ */
+/*
+public class FootballTeam {
+    private int gamesWon;
+    public FootballTeam(int gamesWon){
+        if(gamesWon < 0){
+            throw new IllegalArgumentException( "Not posible to have less than 0 games won");
+
+        }
+        this.gamesWon= gamesWon;
+
+
+    }
+    public int getGamesWon(){
+        return gamesWon;
+    }
+
+}
+ */
+public class FootballTeam implements Comparable<FootballTeam>
+{ private int gamesWon;
+    public FootballTeam(int gamesWon) {
+            this.gamesWon = gamesWon; }
+    public int getGamesWon() {
+        return gamesWon;
+    }
+    @Override
+    public int compareTo(FootballTeam otherTeam) {
+        if (gamesWon > otherTeam.getGamesWon()) {
+            return 1;
+        }
+        else if (gamesWon < otherTeam.getGamesWon()) {
+            return -1;
+        }
+        return 0; }
 }
--- 2024-1/Actividad6-RGR+TDR/src/test/java/FootballTeamTest.java
+++ 2024-1/Actividad6-RGR+TDR/src/test/java/FootballTeamTest.java
@@ -1,7 +1,46 @@
-import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
-/*
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+
 public class FootballTeamTest {
+/*
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 3, 10})
+    void constructorShouldSetGamesWon(int nbOfGamesWon) {
+        FootballTeam team = new FootballTeam(nbOfGamesWon);
+        assertThat(team.getGamesWon())
+                .as("number of games won")
+                .isEqualTo(nbOfGamesWon);
+
+ */
+/*
+        @ParameterizedTest
+        @ValueSource(ints = { -10, -1 })
+        void constructorShouldThrowExceptionForIllegalGamesNb(int illegalNbOfGames) {
+            assertThatExceptionOfType(IllegalArgumentException.class)
+                        .isThrownBy(() -> { new FootballTeam(illegalNbOfGames); });
+
+ */
+/*
+            private static final int ANY_NUMBER = 123;
+            @Test
+            void shouldBePossibleToCompareTeams() {
+                FootballTeam team = new FootballTeam(ANY_NUMBER);
+                assertThat(team).isInstanceOf(Comparable.class);
+            }
+
+ */
+@Test void teamsWithLessMatchesWonShouldBeLesser()
+{
+    FootballTeam team_2 = new FootballTeam(2);
+    FootballTeam team_3 = new FootballTeam(3);
+    assertThat(team_2.compareTo(team_3))
+            .isLessThan(0); }
+}
+/*public class FootballTeamTest {
     @Test
     void constructorShouldSetGamesWon() {
         FootballTeam team = new FootballTeam(3);
@@ -12,6 +51,7 @@ public class FootballTeamTest {
     }
 }*/
 //REFACTORIZAMOS EL CODIGO
+/*
 public class FootballTeamTest {
     private static final int THREE_GAMES_WON = 3;
     @Test
@@ -22,3 +62,6 @@ public class FootballTeamTest {
             .isEqualTo(THREE_GAMES_WON);
        }
 }
+*/
+
+
